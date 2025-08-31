2f9139afa221b4bd85627df4e4c98460c94b972b
adrianmendez140708@gmail.com <adrianmendez140708@gmail.com>
2025-08-31 01:43:26 -0500

    Actualicé todas las pantallas

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 // navigation/AppNavigator.js
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Pantallas
