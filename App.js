064bbf4557caffdc7a251f6d384c9837959bebb8
adrianmendez140708@gmail.com <adrianmendez140708@gmail.com>
2025-10-11 19:02:15 -0500

    Agregué la base de datos de usuarios

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import AppNavigator from './navigation/AppNavigator';
 import dbService from './database'; // <-- Importa el objeto
 import LoadingScreen from './screens/Otros/LoadingScreen';
+import LoginScreen from './screens/Otros/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -25,7 +29,14 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        {/* Agrega aquí las otras pantallas según los roles */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
